47e624f65bf3de310891e33a1a19268e7ec8d8c4
Vignesh Vijiyakumar <146411033+Vignesh200429@users.noreply.github.com>
2025-04-04 18:26:05 +0530

    Calculate for add, sub, mul, and div

---
+++ NewCalc/NewCalc/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NewCalc.Model;
+
+namespace NewCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string oper = "";
+        string firstvalue = "";
+        string secondvalue = "";
+        MathOperation mathOperation;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            mathOperation = new MathOperation();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnnumber = (Button)sender;
+            Numbers(btnnumber.Content.ToString());
+            
+        }
+
+        public bool Numbers(object number)
+        {
+            if (oper == "")
+            {
+                firstvalue = firstvalue + number.ToString();
+                txtresult.Text = firstvalue;
+            }
+            else
+            {
+                secondvalue = secondvalue + number.ToString();
+                txtresult.Text = secondvalue;
+            }
+            return true;
+        }
+
+
+        private void btnclear_Click(object sender, RoutedEventArgs e)
+        {
+            firstvalue = "";
+            secondvalue = "";
+            oper = "";
+            txtresult.Text = "";
+        }
+
+       
+
+        private void Oper_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnoper = (Button)sender;
+            oper = btnoper.Content.ToString();
+        }
+
+        private void btneql_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnnumber = (Button)sender;
+            mathOperation.Add(firstvalue,secondvalue);
+            int result = 0;
+
+            switch (oper)
+            {
+                case "+":
+                    result = mathOperation.Addnumbers(firstvalue , secondvalue);
+                    break;
+                case "-":
+                    result = mathOperation.Subnumbers(firstvalue, secondvalue);
+                    break;
+                case "*":
+                    result = mathOperation.Mulnumbers(firstvalue, secondvalue);
+                    break;
+                case "/":
+                    result = mathOperation.Divnumbers(firstvalue, secondvalue);
+                    break;
+            }
+            txtresult.Text = result.ToString();
+        }
+    }
+}
---
+++ NewCalc/NewCalc/Model/MathOperation.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace NewCalc.Model
+{
+    public class MathOperation
+    {
+         /// <summary>
+         /// used for checking nullorwhitespace and also call the method IsNumeric
+         /// </summary>
+         /// <param name="a"></param>
+         /// <param name="b"></param>
+         /// <returns></returns>         
+        public int Add(string a, string b)
+        {
+            if(string.IsNullOrWhiteSpace(a) || string.IsNullOrWhiteSpace (b))
+            {
+                return 0;
+            }
+            if(!IsNumeric (a) || !IsNumeric (b))
+            {
+                return 0;
+            }
+            return 0;
+        }
+
+
+        /// <summary>
+        ///  Used for Add Two Numbers
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Addnumbers<T>(T a, T b)
+        {
+            
+            return Convert.ToInt32(a) + Convert.ToInt32(b);
+            
+            
+        }
+        /// <summary>
+        /// Used for Sub Two Numbers
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Subnumbers<T>(T a, T b)
+        {
+
+            return Convert.ToInt32(a) - Convert.ToInt32(b);
+
+        }
+        /// <summary>
+        /// Used for Mul Two Numbers
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Mulnumbers<T>(T a, T b)
+        {
+
+            return Convert.ToInt32(a) * Convert.ToInt32(b);
+
+        }
+        /// <summary>
+        /// Used for Div Two Numbers
+        /// </summary>
+        /// <typeparam name="T"> Accept any type</typeparam>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Divnumbers<T>(T a, T b)
+        {
+
+            return Convert.ToInt32(a) / Convert.ToInt32(b);
+
+        }
+        /// <summary>
+        /// Used for Checking Digit or Letter or Both
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool IsNumeric(string value)
+        {
+            foreach (char c in value)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
