306c0ace10e50a603bd467518874ef8afa31439f
Vignesh Vijiyakumar <146411033+Vignesh200429@users.noreply.github.com>
2025-04-24 22:38:16 +0530

    Using Localization for converting English to Tamil

---
+++ Register/Register/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Globalization;
+using System.Windows;
+using Register.Properties;
+
+namespace Register
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Settings os = new Settings();
+            CultureInfo cultureinfo = CultureInfo.GetCultureInfo(os.Language);
+            Thread.CurrentThread.CurrentUICulture = cultureinfo;
+        }
+    }
+
+}
---
+++ Register/Register/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Register
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnsave_Click(object sender, RoutedEventArgs e)
+        {
+            ResourceManager lab = new ResourceManager("Register.Properties.Resources", Assembly.GetExecutingAssembly());
+            string display = lab.GetString("save");
+            MessageBox.Show(display);
+
+            Properties.Settings os = new Properties.Settings();
+            RadioButton first = (RadioButton)stlan.Children[0];
+            RadioButton second = (RadioButton)stlan.Children[1];
+            if (first.IsChecked == true)
+            {
+                os.Language = first.Tag.ToString();
+                os.Save();
+            }
+            if (second.IsChecked == true)
+            {
+                os.Language = second.Tag.ToString();
+                os.Save();
+            }
+        }
+    }
+}
